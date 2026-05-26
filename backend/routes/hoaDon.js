f9852df6e6e129676ff1eaf7738335d9854231f1
thudinh_23521534 <23521534@gm.uit.edu.vn>
2026-05-26 17:08:25 +0700

    Update hoaDon.js

--- backend/routes/hoaDon.js
+++ backend/routes/hoaDon.js
@@ -5,5 +5,6 @@ router.post('/', ctrl.taoHoaDon);
 router.get('/', ctrl.xemTatCa);
 router.get('/:id', ctrl.xemChiTiet);
 router.get('/:id/chi-tiet', ctrl.xemTheoPhieuThue);
+router.delete('/:id', ctrl.xoaHoaDon);
 
 module.exports = router;
