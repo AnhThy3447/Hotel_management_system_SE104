b226c2647c813bf7f47dae20ca96f81555dff8a8
thudinh_23521534 <23521534@gm.uit.edu.vn>
2026-05-27 16:19:58 +0700

    Update thuePhong.js

--- backend/routes/thuePhong.js
+++ backend/routes/thuePhong.js
@@ -6,5 +6,6 @@ router.get('/', ctrl.xemTatCa);
 router.get('/:id', ctrl.xemChiTiet);
 router.put('/:id', ctrl.capNhat);
 router.patch('/:id/tra-phong', ctrl.traPhong);
+router.delete('/:id', ctrl.xoaPhieuThue);
 
 module.exports = router;
