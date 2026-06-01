87eb43841c2ed240e81430c7383871c7734cda52
AnhThy3447 <anhthyvongoc2105@gmail.com>
2026-06-01 07:02:55 +0000

    format folder

--- backend/routes/coQuan.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/coQuanController');
-
-router.post('/', ctrl.them);
-router.get('/', ctrl.xemDanhSach);
-router.get('/:id', ctrl.xemChiTiet);
-router.put('/:id', ctrl.capNhat);
-
-module.exports = router;
